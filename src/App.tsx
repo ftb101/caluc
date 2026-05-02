751768b4a5e515a1faa4b575c34cc561a4aa3593
masashi inoue <jofidtv@gmail.com>
2026-05-02 10:41:47 +0900

    first push

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        
+    </>
+  )
+}
+
+export default App
